d58d3e8ae01050a64ec87c3957825272187a3ae5
jfoltan1 <juliusfoltan@gmail.com>
2022-12-27 22:22:44 +0100

    hurr durr Im norminette and your code sucks lololo

--- ft_split.c
+++ ft_split.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
+
 int	ft_delicount(char const *s, char c)
 {
 	size_t	i;
@@ -21,32 +22,42 @@ int	ft_delicount(char const *s, char c)
 	{
 		if (s[i] == c)
 			f++;
-	i++;
+		i++;
 	}
 	return (f + 2);
 }
+
 int	ft_sizecount(char const *s, char c, size_t i)
 {
 	size_t	d;
+
 	d = 0;
 	while (s[i])
 	{
 		if (s[i] == c)
-			break;
+			break ;
 		d++;
 		i++;
-		
 	}
 	return (d);
 }
+
 char	*ft_alloc(size_t a)
 {
 	char	*ptr;
 
-		ptr = malloc(sizeof (char) * a + 1);
-		if (!ptr)
-			return (NULL);
+	ptr = malloc(sizeof (char) * a + 1);
+	if (!ptr)
+		return (NULL);
 	return (ptr);
+}
+
+char *ft_nullalloc
+{
+	char	*ptr
+	
+	ptr = malloc(sizeof(char));
+	ptr = NULL;
 }
 char	**ft_split(char const *s, char c)
 {
@@ -56,35 +67,31 @@ char	**ft_split(char const *s, char c)
 	size_t	f;
 	char	**ptr;
 
-	ptr = malloc(sizeof(char *) * ft_delicount(s,c));
+	ptr = malloc(sizeof(char *) * ft_delicount(s, c));
 	if (!ptr)
 		return (NULL);
 	f = i = b = 0;
-	a = ft_delicount(s,c);
-	while(a > 0)
+	a = ft_delicount(s, c);
+	while (a > 0)
 	{
 		if (a != 1)
-		ptr[f] = ft_alloc(ft_sizecount(s,c,i));
+			ptr[f] = ft_alloc(ft_sizecount(s, c, i));
 		if (a == 1)
-			{
-			ptr[f] = malloc(sizeof(char));
-			ptr[f] = NULL;
-			}
+			ptr[f] = ft_nullalloc;
 		while (s[i] != c && s[i])
-		{
+		{	
 			if (s[i] != c)
 				ptr[f][b] = s[i];
-		i++;	
-		b++;
+			i++, x++;
 		}
-	i++;
-	if (a != 1)
-		ptr[f][b] = 0;
-	a--;
-	b = 0;
-	f++;
+		i++;
+		if (a != 1)
+			ptr[f][b] = 0;
+		a--;
+		b = 0;
+		f++;
 	}
-	return(ptr);
+	return (ptr);
 }
 /*#include <stdio.h>
 int main()
